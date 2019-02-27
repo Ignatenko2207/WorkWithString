727ddce7e38510cf7e4d650dd2f2fa060fc920e4
Alex <ignatenko2207@gmail.com>
2019-02-27 19:56:47 +0200

    Initial commit

---
+++ src/ua/com/qalight/AppRunner.java
@@ -0,0 +1,12 @@
+package ua.com.qalight;
+
+import java.util.logging.Logger;
+
+public class AppRunner {
+
+	private static Logger logger = Logger.getLogger(AppRunner.class.getName());
+	
+	public static void main(String[] args) {
+		
+	}
+}
