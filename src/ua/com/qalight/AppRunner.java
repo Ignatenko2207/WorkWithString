6225bd83677088ec93c604d9d5d251b11788fe42
Alex <ignatenko2207@gmail.com>
2019-03-01 21:22:40 +0200

    Add comment in Russian

--- src/ua/com/qalight/AppRunner.java
+++ src/ua/com/qalight/AppRunner.java
@@ -8,8 +8,30 @@ public class AppRunner {
 	
 	public static void main(String[] args) {
 
-
+		// комментарий на русском
 		
+		String name = "Alex";
 		
+		name = name + " Ignatenko";
+		
+		logger.info(name);
+		
+		/*
+		 * 25899852
+		 * 28582
+		 * 258852
+		 * 12345
+		 * 259652 -> "259652" == "256952"
+		 */
+		
+		StringBuilder nameBuilder = new StringBuilder("Alex");
+			
+		nameBuilder = nameBuilder.reverse();
+		
+		StringBuffer nameBuffer = new StringBuffer("Alex");
+		
+		nameBuffer = nameBuffer.reverse();
+		
+		logger.info(nameBuilder.toString());
 	}
 }
