47bab2610eef0054d931fc677d91482b732a529e
Alex <ignatenko2207@gmail.com>
2019-03-02 00:25:57 +0200

    Add new examples of string manipulation

--- src/ua/com/qalight/AppRunner.java
+++ src/ua/com/qalight/AppRunner.java
@@ -1,20 +1,37 @@
 package ua.com.qalight;
 
+import java.io.UnsupportedEncodingException;
 import java.util.logging.Logger;
 
 public class AppRunner {
 
 	private static Logger logger = Logger.getLogger(AppRunner.class.getName());
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
 
 		// комментарий на русском
 		
-		String name = "Alex";
+		String str = "My name 254833 is \"Alex\" and крякоЗябЛик other %1s symbols %2s %3d"
+				+ "\\"
+				+ "\t"
+				+ "\n"
+				+ "\r";
+		byte[] bytes = str.getBytes();
 		
-		name = name + " Ignatenko";
 		
-		logger.info(name);
+//		String encStr = new String(bytes, "UTF-8");
+		
+		str = str + " Ignatenko";
+		
+		String formatedstr = str.format(str, "name", "lastName", 56);
+		String editedStr = str.replaceAll("\"", "");
+//		String editedStr = str.replaceAll("(.+\\d)(33)", "$1");
+//		String editedStr = str.replaceAll("[a-zA-Z]", "");
+				
+		// My name is "Alex"
+		
+		logger.info(str);
+		logger.info(formatedstr);
 		
 		/*
 		 * 25899852
@@ -24,14 +41,14 @@ public class AppRunner {
 		 * 259652 -> "259652" == "256952"
 		 */
 		
-		StringBuilder nameBuilder = new StringBuilder("Alex");
-			
-		nameBuilder = nameBuilder.reverse();
-		
-		StringBuffer nameBuffer = new StringBuffer("Alex");
-		
-		nameBuffer = nameBuffer.reverse();
-		
-		logger.info(nameBuilder.toString());
+//		StringBuilder nameBuilder = new StringBuilder("Alex");
+//			
+//		nameBuilder = nameBuilder.reverse();
+//		
+//		StringBuffer nameBuffer = new StringBuffer("Alex");
+//		
+//		nameBuffer = nameBuffer.reverse();
+//		
+//		logger.info(nameBuilder.toString());
 	}
 }
